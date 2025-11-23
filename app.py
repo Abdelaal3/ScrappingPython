0f1474a50053576e97d6cdae1c837ef25e12d236
Abdelaal3 <mahmoudabdelaal672@gmail.com>
2025-11-23 02:17:55 +0200

    Edit File 2

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
 import random
+import os
+
 
 app = Flask(__name__)
 
@@ -90,4 +92,4 @@ def matches():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
