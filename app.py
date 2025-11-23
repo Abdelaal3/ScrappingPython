edc67d440c9841fc8898168a22ba3d1122465128
Mahmoud Abdelaal <mahmoudabdelaal672@gmail.com>
2025-11-24 00:53:43 +0200

    Correct League Name

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import json
-import os
 from scraper import scrape_filgoal_matches
-
-
-
-if os.path.exists("daily_matches.json"):
-    os.remove("daily_matches.json")
 
 app = Flask(__name__)
 CORS(app)
@@ -26,21 +19,11 @@ def home():
 
 @app.route("/matches")
 def get_matches():
-    date = request.args.get("date")
-
-    # لو تاريخ محدد → scrape مرة واحدة
-    if date:
+        date = request.args.get("date")
         return jsonify(scrape_filgoal_matches(date)), 200
-
-    # لو تاريخ اليوم → اقرأ من الملف
-    if os.path.exists("daily_matches.json"):
-        with open("daily_matches.json", "r", encoding="utf-8") as f:
-            return jsonify(json.load(f)), 200
-
-    # fallback — لو الملف مش موجود
-    return jsonify(scrape_filgoal_matches()), 200
 
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
